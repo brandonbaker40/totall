213c3e0143559516c86272bf5d132e25b43cfe39
Mike Berkman <mberkman@covermymeds.com>
2018-11-24 16:10:24 -0500

    Category routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get  '/help',      to:   'static_pages#help'
   get  '/about',     to:   'static_pages#about'
   get  '/contact',   to:   'static_pages#contact'
+  get  '/help',      to:   'static_pages#help'
 
   get   '/signup',    to:   'users#new'
   post  '/signup',    to:   'users#create'
@@ -10,14 +10,12 @@ Rails.application.routes.draw do
   post  '/login',     to:   'sessions#create'
   get   '/logout',    to:   'sessions#destroy'
 
+  resources :categories
   resources :users do
     resources :accounts do
       resources :balances
     end
   end
-
-  resources :categories
-  resources :update
 
   root  'static_pages#home'
 end
