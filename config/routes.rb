3960bcdb5ac608144261d4f8ead38b003c5be69d
Mike Berkman <mberkman@covermymeds.com>
2018-11-24 15:42:14 -0500

    removed update assets

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get   '/signup',    to:   'users#new'
   post  '/signup',    to:   'users#create'
 
-  get   '/update',    to:   'update#index'
   get   '/login',     to:   'sessions#new'
   post  '/login',     to:   'sessions#create'
   get   '/logout',    to:   'sessions#destroy'
